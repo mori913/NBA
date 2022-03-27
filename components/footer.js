93c534bfc13aa8e76dac91417346fe80b287a485
mye71 <yemaosen@g.ecc.u-tokyo.ac.jp>
2022-03-27 16:00:31 +0800

    edit the us map

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <Box align="center" opacity={0.4} fontsize="sm" >
+    <Box align="center" opacity={0.4} fontSize="sm" >
       &copy; {new Date().getFullYear()} Team 143. All Rights Reserved.
     </Box>
   )
