4ed0bcaca7dcfd600b988adfc383cb7575d4f70a
mori <yemaosen@g.ecc.u-tokyo.ac.jp>
2022-03-30 20:29:12 +0800

    add player picture

--- components/playertable.js
+++ components/playertable.js
@@ -8,57 +8,76 @@ import {
   Th,
   Td,
   Select,
-  Input
+  Input,
+  HStack,
+  Avatar,
+  Text
 } from '@chakra-ui/react'
-import { Image } from '@chakra-ui/react'
-import teammap from '/lib/teammap'
 import * as d3 from 'd3'
 import { SimpleGrid } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 
 const PlayerTable = () => {
-  console.log('test')
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [players, setPlayers] = useState([])
 
-  d3.json('http://data.nba.net/data/10s/prod/v1/2019/players.json').then(
-    function (d) {
-      console.log(d.league.standard)
-    }
-  )
+  useEffect(() => {
+    d3.json('http://data.nba.net/data/10s/prod/v1/2019/players.json')
+      .then(function (d) {
+        setIsLoaded(true)
+        const data = d.league.standard.filter(elem => elem.teamId > 0)
+        console.log(data)
+        setPlayers(data)
+      })
+      .catch(error => {
+        setIsLoaded(true)
+        setError(error)
+      })
+  }, [])
+
   return (
     <Container maxW="container.lg">
       <Heading as="h3" size="md" pb="4">
         All Players
       </Heading>
-      <SimpleGrid minChildWidth="120px" spacing="30px" pb={4} >
-        <Select placeholder='All Teams' bg="#FFF"/>
-        <Select placeholder='All Positions' bg="#FFF"/>
+      <SimpleGrid minChildWidth="120px" spacing="30px" pb={4}>
+        <Select placeholder="All Teams" bg="#FFF" />
+        <Select placeholder="All Positions" bg="#FFF" />
         <Select placeholder="2022" bg="#FFF"></Select>
-        <Input placeholder='Search Players' bg = "#FFF"/>
+        <Input placeholder="Search Players" bg="#FFF" />
       </SimpleGrid>
       <Table variant="simple" bg="#FFFFFF" borderRadius="xl">
         <Thead>
           <Tr>
             <Th>Player</Th>
             <Th>Team</Th>
-            <Th>Number</Th>
             <Th>Position</Th>
+            <Th>Number</Th>
           </Tr>
         </Thead>
         <Tbody>
-          {Object.keys(teammap).map(key => {
-            const img = 'teams/' + key + '.png'
+          {players.map((elem, index) => {
+            let name = elem.firstName + ' ' + elem.lastName
+            let img =
+              'https://cdn.nba.com/headshots/nba/latest/260x190/' +
+              elem.personId +
+              '.png'
             return (
-              <Tr key={key}>
+              <Tr key={index}>
                 <Td>
-                  <Image
-                    borderRadius="full"
-                    boxSize="60px"
-                    src={img}
-                    alt="Dan Abramov"
-                  />
+                  <HStack spacing="3" >
+                    <Avatar
+                      name={name}
+                      src={img}
+                      bg ="#FFF"
+                    />
+                  <Text fontWeight="medium">{name}</Text>
+                  </HStack>
                 </Td>
-                <Td>{teammap[key].team}</Td>
-                <Td>{teammap[key].city}</Td>
-                <Td>1</Td>
+                <Td>{elem.teamId}</Td>
+                <Td>{elem.pos}</Td>
+                <Td>{elem.jersey}</Td>
               </Tr>
             )
           })}
--- pages/players/index.js
+++ pages/players/index.js
@@ -1,4 +1,4 @@
-import { Container, Flex, Text, VStack } from '@chakra-ui/react'
+import { Container, isMobile, ButtonGroup, Button, Text, HStack,Box } from '@chakra-ui/react'
 import Layout from '../../components/layouts/team'
 import PlayerTable from '../../components/playertable'
 const Teams = () => {
@@ -6,6 +6,33 @@ const Teams = () => {
     <Layout>
       <Container maxW="container.lg" pt={40} pb={5} >
         <PlayerTable/>
+        <Box
+            px={{
+              base: '4',
+              md: '6',
+            }}
+            pb="5"
+          >
+            <HStack spacing="3" justify="space-between">
+              {!isMobile && (
+                <Text color="muted" fontSize="sm">
+                  Showing 1 to 5 of 42 results
+                </Text>
+              )}
+              <ButtonGroup
+                spacing="3"
+                justifyContent="space-between"
+                width={{
+                  base: 'full',
+                  md: 'auto',
+                }}
+                variant="secondary"
+              >
+                <Button>Previous</Button>
+                <Button>Next</Button>
+              </ButtonGroup>
+            </HStack>
+          </Box>
       </Container>
     </Layout>
   )
